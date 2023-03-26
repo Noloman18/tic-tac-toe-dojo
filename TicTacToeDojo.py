91a4fe285de0559a1e57af7b23f07e6a5600ff6e
Letlhogonolo Segoe <noloxtreme@gmail.com>
2023-03-26 17:09:48 +0200

    Saving models

--- TicTacToeDojo.py
+++ TicTacToeDojo.py
@@ -1,9 +1,10 @@
+import os
+import pickle
 import random
+import time
+
 import numpy as np
 import tensorflow as tf
-import pickle
-import os
-import time
 
 # Define the state space
 all_possible_boards = [' ' * 9]
@@ -20,9 +21,9 @@ all_possible_boards.sort()
 # Define the action space
 all_possible_actions = [i for i in range(9)]
 
-def convert_tuple_board_to_numeric(state_tuple):
+def convert_to_numeric(state_tuple):
     MY_DICT = {'X':0, 'O':1, ' ':2}
-    return [MY_DICT[i] for i in list(state_tuple)]
+    return [MY_DICT[i] for i in state_tuple]
 
 def print_board(board):
     print(" "+board[0]+" | "+board[1]+" | "+board[2]+" ")
@@ -50,14 +51,13 @@ def is_winner(board, player):
     # If no winner is found, return False
     return False
 
-def is_game_over(board, turn):
-    return turn > 9 or ' ' not in board or is_winner(board, 'X') or is_winner(board, 'O')
+def is_game_over(board):
+    return ' ' not in board or is_winner(board, 'X') or is_winner(board, 'O')
 
 def get_other_player(player):
     return 'O' if player == 'X' else 'X'
 
-def get_all_possible_actions(state):
-    board, player, turn = state
+def get_all_possible_actions(board):
     return [i for i in range(len(board)) if board[i] == ' ']
 
 def select_random(actions):
@@ -67,15 +67,15 @@ def select_random(actions):
         raise IndexError;
 
 # Define the reward function
-def reward(previous_state, next_state):
-    board, player, turn = next_state
-    if is_winner(board, player):
-        return 2.0
-    elif is_winner(board, get_other_player(player)):
+def reward(state, next_state):
+    if len(get_all_possible_actions(state[0])) == len(get_all_possible_actions(next_state[0])):
         return -2.0
 
-    if len(get_all_possible_actions(previous_state)) == len(get_all_possible_actions(next_state)):
-        return -0.2
+    if is_winner(next_state[0], state[1]):
+        return 2.0
+
+    if is_winner(next_state[0], next_state[1]):
+        return -2.0
 
     return 0.0
 
@@ -93,26 +93,26 @@ class QFunction:
     def _build_model(self):
         inputs = tf.keras.layers.Input(shape=self.state_shape)
         x = tf.keras.layers.Dense(self.hidden_units, activation='relu')(inputs)
-        x = tf.keras.layers.Dense(self.hidden_units, activation='relu')(x)
+        # x = tf.keras.layers.Dense(self.hidden_units, activation='relu')(x)
         outputs = tf.keras.layers.Dense(self.num_actions)(x)
         model = tf.keras.models.Model(inputs=inputs, outputs=outputs)
         return model
 
     def __call__(self, state):
-        state = np.array(convert_tuple_board_to_numeric(state[0]))
+        state = np.array(convert_to_numeric(state[0]))
         state = state.reshape(1, *state.shape)
         q_values = self.model.predict(state)[0]
         return q_values
 
-    def update(self, state, action, target):
-        state = np.array(convert_tuple_board_to_numeric(state[0]))
+    def update(self, state, action, q_value_new):
+        state = np.array(convert_to_numeric(state[0]))
         state = state.reshape(1, *state.shape)
         action_one_hot = np.zeros(self.num_actions)
         action_one_hot[action] = 1
         with tf.GradientTape() as tape:
-            q_values = self.model(state)
-            q_value = tf.reduce_sum(tf.multiply(q_values, action_one_hot))
-            loss = tf.square(target - q_value)
+            q_value_old = self.model(state)
+            q_value = tf.reduce_sum(tf.multiply(q_value_old, action_one_hot))
+            loss = tf.square(q_value_new - q_value)
         gradients = tape.gradient(loss, self.model.trainable_variables)
         self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
 
@@ -130,14 +130,21 @@ class Agent:
         self.epsilon = epsilon
         self.actions_performed = {'X': [], 'O': []}
 
-    def act(self, state):
-        if random.random() < self.epsilon:
-            action = select_random(get_all_possible_actions(state))
+    def act(self, state, skipRandom = False):
+        notRandomSelection = True
+        actions = get_all_possible_actions(state[0])
+        if not skipRandom and random.random() < self.epsilon:
+            action = select_random(actions)
+            notRandomSelection = False
+            while True:
+                if action not in actions:
+                    action = select_random(actions)
+                else:
+                    break
         else:
             q_values = self.q_function(state)
             action = np.argmax(q_values)
-            if self.epsilon == 0:
-                actions = get_all_possible_actions(state)
+            if skipRandom and self.epsilon == 0:
                 while True:
                     action = np.argmax(q_values)
                     if action not in actions:
@@ -145,8 +152,8 @@ class Agent:
                     else:
                         break
 
-        self.actions_performed.get(state[1]).append(action)
-        return action
+        self.actions_performed.get(state[1]).append(f"{'NR' if notRandomSelection else 'R'} {action}")
+        return action, np.max(self.q_function(state))
 
     def learn(self, state, action, reward, next_state):
         next_q_values = self.q_function(next_state)
@@ -163,7 +170,7 @@ class Agent:
         self.actions_performed = {'X':[], 'O':[]}
 
 def reload_memento(agent):
-    training_context = {'elapsed_time_in_seconds': 0, 'num_iterations' : 0}
+    training_context = {'elapsed_time_in_seconds': 0, 'num_iterations' : 0, 'running_total_q_values':0}
     if os.path.exists('./model/tic_tac_toe_agent.pkl'):
         with open('./model/tic_tac_toe_agent.pkl', 'rb') as f:
             model = pickle.load(f)
@@ -182,31 +189,35 @@ def create_memento(agent, training_context):
 def perform_tic_tac_toe_training():
     # Train the agent
     training_instance_iterations = 10_000_000
-    agent = Agent(state_shape=(9,), num_actions=9, hidden_units=64, learning_rate=0.001, gamma=0.99, epsilon=0.1)
+    agent = Agent(state_shape=(9,), num_actions=9, hidden_units=64, learning_rate=0.01, gamma=0.99, epsilon=0.1)
 
     training_context = reload_memento(agent)
 
     original_elapsed_time = training_context['elapsed_time_in_seconds']
     st = time.time()
+
+    running_q_value = 0
     for episode in range(training_context['num_iterations'],training_instance_iterations):
         board = [' ' for _ in range(9)]
         player = 'X'
-        turn = 1
         agent.new_game()
         training_context['num_iterations'] += 1
-        while not is_game_over(board, turn):
-            state = (tuple(board), player, turn)
-            action = agent.act(state)
+        turns = 0
+        while not is_game_over(board):
+            state = (board[:], player)
+            action, q_value  = agent.act(state)
+            running_q_value += q_value
             board[action] = player
-            turn += 1
-            next_state = (tuple(board), get_other_player(player), turn)
+            turns +=1
+            next_state = (board[:], get_other_player(player))
             reward_value = reward(state, next_state)
             agent.learn(state, action, reward_value, next_state)
             player = get_other_player(player)
             elapsed_time = time.time() - st
             training_context['elapsed_time_in_seconds'] = original_elapsed_time + elapsed_time
-        if episode%10 == 0:
-            print("Episode", episode, "Game:", agent.actions_performed, "Training context", training_context)
+        if episode > 0 and episode%10 == 0:
+            print("Episode", episode, "Game:", agent.actions_performed, "Training context", training_context, f"Average Q value {running_q_value/10:.2f}", "Turns ",turns)
+            running_q_value = 0
         if episode > 0 and episode %100 ==0:
             create_memento(agent, training_context)
 
@@ -219,10 +230,10 @@ def play_simulation():
     # Play the game
     board = [' ' for _ in range(9)]
     player = 'X'
-    turn = 1
-    while not is_game_over(board):
-        state = (tuple(board), player, turn)
-        action = agent.act(state)
+    turn = 0
+    while not is_game_over(board,turn):
+        state = (board[:], player)
+        action,q_value = agent.act(state,True)
         board[action] = player
         turn += 1
         player = get_other_player(player)
